5b0ce693091c1dea735748a19c09f143a32f71e0
@sillasgoes <sillasgoes@gmail.com>
2021-08-30 16:58:29 -0300

    Alteracoes no front de pedidos

--- src/components/Pedidos.jsx
+++ src/components/Pedidos.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import PageTitle from '../../src/views/PageTitle';
+import './layout/Pedidos.css'
+
 
 const Pedidos =  (props) => {
    return (
-       <div>
-           Pedidos
+       <div className="princpal">
+           <PageTitle title="Pedidos"/>
+             <div className="Content_right">
+             <table className="table_1">
+                 <thead>
+                     <tr>
+                         <th>Nº Pedido</th>
+                         <th>Descrição</th>
+                         <th>Data</th>
+                         <th>Setor</th>
+                     </tr>
+                 </thead>
+                 <tbody>
+                     <tr>
+                         <td>10</td>
+                         <td>2 - Armários softbase </td>
+                         <td>24/06/2021 - 15:50:34 </td>
+                         <td>Vendas</td>
+                     </tr>
+                 </tbody>
+             </table>
+             </div>
        </div>
    )
 }
